dbfd69bc803dd3578a7af18921cca3fd90220241
Wishable <gjbbub05@gmail.com>
2023-04-02 20:22:56 -0600

    Add functionality to add books to screen

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
 let myLibrary = [];
+const library = document.querySelector('.library');
+const form = document.querySelector('form#addnewbook');
 function Book(title, coverPage, author, pages, read) {
     this.title = title;
+    this.coverPage = coverPage;
     this.author = author;
     this.pages = pages;
     this.read = read;
@@ -11,6 +14,83 @@ Book.prototype.info = function info() {
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
-const thehobbit = new Book('The Hobbit', 'https://prodimage.images-bn.com/pimages/9780345445605_p0_v1_s550x406.jpg', 'J.R.R Tolken', 295, false);
-addBookToLibrary(thehobbit);
-console.log(myLibrary);
+function createButtonsDiv() {
+    const buttonsDiv = document.createElement('div');
+    buttonsDiv.classList.add('buttons');
+    const hasReadButton = document.createElement('button');
+    const deleteButton = document.createElement('button');
+    const trashIcon = document.createElement('img');
+    hasReadButton.classList.add('has-read');
+    deleteButton.classList.add('delete');
+    trashIcon.setAttribute('src', 'assets/trash-can-outline.svg');
+    trashIcon.setAttribute('alt', 'delete');
+    deleteButton.appendChild(trashIcon);
+    buttonsDiv.appendChild(hasReadButton);
+    buttonsDiv.appendChild(deleteButton);
+    return buttonsDiv;
+}
+function clearLibrary() {
+    while (library.firstChild) {
+        library.removeChild(library.firstChild);
+    }
+}
+function displayBooks(books) {
+    clearLibrary();
+    books.forEach((book, index) => {
+        const bookElement = document.createElement('div');
+        bookElement.classList.add('book');
+        bookElement.setAttribute('data-index', String(index));
+        const bookTitleElement = document.createElement('h1');
+        bookTitleElement.classList.add('book-title');
+        const coverPageElement = document.createElement('img');
+        coverPageElement.classList.add('cover-page');
+        const bookAuthorElement = document.createElement('p');
+        bookAuthorElement.classList.add('book-author');
+        const bookPagesElement = document.createElement('p');
+        bookPagesElement.classList.add('book-pages');
+        const buttonsDiv = createButtonsDiv();
+        const hasRead = buttonsDiv.firstChild;
+        Object.entries(book).map(([key, value]) => {
+            if (key === 'title') {
+                bookTitleElement.textContent = String(value);
+            }
+            if (key === 'coverPage') {
+                coverPageElement.setAttribute('src', String(value));
+            }
+            if (key === 'author') {
+                bookAuthorElement.textContent = String(value);
+            }
+            if (key === 'pages') {
+                bookPagesElement.textContent = `Pages: ${String(value)}`;
+            }
+            if (key === 'read') {
+                hasRead.textContent = Boolean(value) ? 'Read' : 'Not Read';
+            }
+        });
+        bookElement.appendChild(bookTitleElement);
+        bookElement.appendChild(coverPageElement);
+        bookElement.appendChild(bookAuthorElement);
+        bookElement.appendChild(bookPagesElement);
+        bookElement.appendChild(buttonsDiv);
+        library.appendChild(bookElement);
+    });
+}
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const bookTitle = form.querySelector('input[name="title"]').value;
+    const bookCoverPage = form.querySelector('input[name="cover"]').value;
+    const bookAuthor = form.querySelector('input[name="author"]').value;
+    const bookPages = form.querySelector('input[name="pages"]').value;
+    const bookHasRead = form.querySelector('input[name="hasread"]').value;
+    // if (bookHasRead === '1') {
+    //   addBookToLibrary(
+    //     new Book(bookTitle, bookCoverPage, bookAuthor, parseInt(bookPages), true)
+    //   );
+    // } else {
+    //   addBookToLibrary(
+    //     new Book(bookTitle, bookCoverPage, bookAuthor, parseInt(bookPages), false)
+    //   );
+    // }
+    // displayBooks(myLibrary);
+    console.log(bookHasRead);
+});
